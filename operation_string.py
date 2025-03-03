3823312085e7f7c690214dc9a60a4531cb190186
camillegil <gilcamille022@gmail.com>
2025-03-03 13:42:17 +0100

    Added string slices

--- operation_string.py
+++ operation_string.py
@@ -1,25 +1,23 @@
 s1 = "hello"
 s2 = "world"
-print(s1 + s2)
-#add a space without modifying s1 and s2
 print(s1 + " " + s2)
+print(s1, "hello")
 print(3*s2)
 print((10//2)*s1)
-
-#len function gives you the size of the string
+# len function gives you the size of the string
 print(s1, len(s1))
 print(3*s2, len(3*s2))
 for c in s2:
     print(c*4, end="")
-#or
+print()
+
 new_string = ""
 for c in s2:
-    new_string += c*4
+    new_string += c*4 # same as: new_string = new_string + 4 * c
 print(new_string)
 
-#in can be used to check if one string is inside another
-print("h" in s1)#true
-print("d" in s2)#true
-print("x" in s1)#false
-print("wor" in s2)#true
-
+# in can be used to check if one stirng is inside another
+print("h" in s1) # true
+print("d" in s2) # true
+print("x" in s1) # false
+print("wor" in s2) # true
--- string_methods.py
+++ string_methods.py
@@ -1,47 +1,47 @@
-from os.path import split
-
 print(dir("hello"))
 
 print(help("hi".capitalize))
 
-print("I like to go to school". capitalize())
+print("i like to go to school".capitalize())
 print("IS THIS SUPPOSED TO WORK?".capitalize())
-print("hello".center(40, "*")) # nothing, "*" or "-"
-print("gmail.com".find(".")) #if doesn't find -> -1
+print("hello".center(50, "-"))
+print("gmail.com".find("."))
+print("gmail.com".find("john"))
 s = "i see a cat who like to cat while i cat on a cat"
-#find all occurrences
+# find all occurrences
 poz = 0
 while True:
     poz = s.find("cat", poz)
     if poz == -1:
         break
-    print("fund cat on position", poz)
+    print("found cat on position", poz)
     poz += 1
 
-#lower
-s = "I SEE A LOT OF THINGS"
+# join - we will come back later
+
+# lower
+s = "I SEE A LOT OF THINGS!"
 print(s.lower())
 
-#replace - replace x with y
-s = "i see a cat who likes to eat a rat, what a good cat"
+# replace - replaces X with Y
+s = "i see a cat who likes to eat a rat. what a good cat"
 print(s.replace("c", "r"))
 print(s.replace("cat", "lion"))
-s = "hello, kind sir!"
-print(s.replace(",", ""))
+s = "Hello, kind sir! How are you today?"
+print(s.replace(",", "").replace("!", "").replace("?", ""))
 
-#split
-s = "i like to go shopping"
+# split
+s = "I like to go shopping"
 print(s.split())
 splitted = s.split()
-print("XX".join(splitted)) #join
+print("XX".join(splitted))
 
-#upper
-s = "i see a lot of things"
-print(s.upper())
-print(s.upper().capitalize())
-
-#tittle
+# title
 s = "i like the mountains"
 print(s.title())
+
+# upper
+s = "i see a lot of things"
+print(s.upper().capitalize())
 
 
